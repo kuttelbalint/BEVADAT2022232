88ea866bbedde2cacaa5033995c698d88e8b397d
kuttelbalint <106156156+kuttelbalint@users.noreply.github.com>
2023-03-12 17:37:34 +0100

    hazi02 WIP

---
+++ HAZI/HAZI02/HAZI02.py
@@ -0,0 +1,181 @@
+# %%
+import numpy as np
+import datetime
+
+# %%
+#FONTOS!!!
+
+# CSAK OTT LEHET HASZNÁLNI FOR LOOP-OT AHOL A FELADAT KÜLÖN KÉRI!
+# [1,2,3,4] --> ezek az értékek np.array-ek. Ahol listát kérek paraméterként ott külön ki fogom emelni!
+# Ha végeztél a feladatokkal, akkor notebook-ot alakítsd át .py.
+# A FÁJLBAN CSAK A FÜGGVÉNYEK LEGYENEK! (KOMMENTEK MARADHATNAK)
+
+# %%
+# Írj egy olyan fügvényt, ami megfordítja egy 2d array oszlopait. Bemenetként egy array-t vár.
+# Be: [[1,2],[3,4]]
+# Ki: [[2,1],[4,3]]
+# column_swap()
+
+def column_swap(arr):
+    arr[:, [0,1]] = arr[:, [1,0]]
+    return arr
+
+arr = np.array([[1,2], [3,4]])
+
+print(column_swap(arr))
+
+# %%
+# Készíts egy olyan függvényt ami összehasonlít két array-t és adjon vissza egy array-ben, hogy hol egyenlőek 
+# Pl Be: [7,8,9], [9,8,7] 
+# Ki: [1]
+# compare_two_array()
+# egyenlő elemszámúakra kell csak hogy működjön
+
+def compare_two_array(arr1, arr2):
+    equal_indices = np.where(arr1 == arr2)[0]
+    return equal_indices
+
+# %%
+# Készíts egy olyan függvényt, ami vissza adja string-ként a megadott array dimenzióit:
+# Be: [[1,2,3], [4,5,6]]
+# Ki: "sor: 2, oszlop: 3, melyseg: 1"
+# get_array_shape()
+# 3D-vel még műküdnie kell!, 
+
+def get_array_shape(arr):
+    shape = arr.shape
+    rank = arr.ndim
+    depth = 1
+    if rank == 3:
+        depth = shape[2]
+
+    return "sor: " + str(shape[0]) + ", oszlop: " + str(shape[1]) + ", melyseg: " + str(depth)
+
+# %%
+# Készíts egy olyan függvényt, aminek segítségével elő tudod állítani egy neurális hálózat tanításához szükséges pred-et egy numpy array-ből. 
+# Bementként add meg az array-t, illetve hogy mennyi class-od van. Kimenetként pedig adjon vissza egy 2d array-t, ahol a sorok az egyes elemek. Minden nullákkal teli legyen és csak ott álljon egyes, ahol a bementi tömb megjelöli. 
+# Pl. ha 1 van a bemeneten és 4 classod van, akkor az adott sorban az array-ban a [1] helyen álljon egy 1-es, a többi helyen pedig 0.
+# Be: [1, 2, 0, 3], 4
+# Ki: [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
+# encode_Y()
+
+def encode_Y(input_array, num_rows):
+    output_array = np.zeros((num_rows, num_rows), dtype=int)
+    row_indices = np.arange(num_rows)
+    output_array[row_indices, input_array] = 1
+    return output_array
+
+# %%
+# A fenti feladatnak valósítsd meg a kiértékelését. Adj meg a 2d array-t és adj vissza a decodolt változatát
+# Be:  [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
+# Ki:  [1, 2, 0, 3]
+# decode_Y()
+
+
+
+def decode_Y(output_array):
+    num_rows, num_cols = output_array.shape
+    row_indices = np.arange(num_rows)
+    col_indices = np.argmax(output_array, axis=1)
+    decoded_array = col_indices[row_indices]
+    return np.array(decoded_array)
+
+# %%
+# Készíts egy olyan függvényt, ami képes kiértékelni egy neurális háló eredményét! Bemenetként egy listát és egy array-t és adja vissza azt az elemet, aminek a legnagyobb a valószínüsége(értéke) a listából.
+# Be: ['alma', 'körte', 'szilva'], [0.2, 0.2, 0.6]. # Az ['alma', 'körte', 'szilva'] egy lista!
+# Ki: 'szilva'
+# eval_classification()
+
+def eval_classification(list_, arr):
+    max_probability = np.argmax(arr)
+    return list_[max_probability]
+
+# %%
+# Készíts egy olyan függvényt, ahol az 1D array-ben a páratlan számokat -1-re cseréli
+# Be: [1,2,3,4,5,6]
+# Ki: [-1,2,-1,4,-1,6]
+# replace_odd_numbers()
+
+def replace_odd_numbers(arr):
+    arr[arr % 2 != 0] = -1
+    return arr
+
+# %%
+# Készíts egy olyan függvényt, ami egy array értékeit -1 és 1-re változtatja, attól függően, hogy az adott elem nagyobb vagy kisebb a paraméterként megadott számnál.
+# Ha a szám kisebb mint a megadott érték, akkor -1, ha nagyobb vagy egyenlő, akkor pedig 1.
+# Be: [1, 2, 5, 0], 2
+# Ki: [-1, 1, 1, -1]
+# replace_by_value()
+
+def replace_by_value(arr, num):
+    outputArr = np.where(arr >= num, 1, -1)
+    return outputArr
+
+# %%
+# Készíts egy olyan függvényt, ami egy array értékeit összeszorozza és az eredményt visszaadja
+# Be: [1,2,3,4]
+# Ki: 24
+# array_multi()
+# Ha több dimenziós a tömb, akkor az egész tömb elemeinek szorzatával térjen vissza
+
+def array_multi(arr):
+    return np.prod(arr)
+
+# %%
+# Készíts egy olyan függvényt, ami egy 2D array értékeit összeszorozza és egy olyan array-el tér vissza, aminek az elemei a soroknak a szorzata
+# Be: [[1, 2], [3, 4]]
+# Ki: [2, 12]
+# array_multi_2d()
+
+def array_multi_2d(arr):
+    return np.prod(arr, axis=1)
+
+# %%
+# Készíts egy olyan függvényt, amit egy meglévő numpy array-hez készít egy bordert nullásokkal. Bementként egy array-t várjon és kimenetként egy array jelenjen meg aminek van border-je
+# Be: [[1,2],[3,4]]
+# Ki: [[0,0,0,0],[0,1,2,0],[0,3,4,0],[0,0,0,0]]
+# add_border()
+
+def add_border(arr):
+    m, n = arr.shape
+    output = np.zeros((m+2, n+2), dtype=arr.dtype)
+    output[1:-1, 1:-1] = arr
+    return output
+
+# %%
+# A KÖTVETKEZŐ FELADATOKHOZ NÉZZÉTEK MEG A NUMPY DATA TYPE-JÁT!
+
+# %%
+# Készíts egy olyan függvényt ami két dátum között felsorolja az összes napot és ezt adja vissza egy numpy array-ben. A fgv ként str vár paraméterként 'YYYY-MM' formában.
+# Be: '2023-03', '2023-04'  # mind a kettő paraméter str.
+# Ki: ['2023-03-01', '2023-03-02', .. , '2023-03-31',]
+# list_days()
+
+from datetime import datetime
+
+
+
+# %%
+# Írj egy fügvényt ami vissza adja az aktuális dátumot az alábbi formában: YYYY-MM-DD. Térjen vissza egy 'numpy.datetime64' típussal.
+# Be:
+# Ki: 2017-03-24
+# get_act_date()
+
+def get_act_date():
+    current_date = np.datetime64('today')
+    return current_date
+
+# %%
+# Írj egy olyan függvényt ami visszadja, hogy mennyi másodperc telt el 1970 január 01. 00:02:00 óta. Int-el térjen vissza
+# Be: 
+# Ki: másodpercben az idó, int-é kasztolva
+# sec_from_1970()
+
+def sec_from_1970():
+    converted_time = datetime.datetime(1970, 1, 1, 0, 2, 0)
+    current_time = datetime.datetime.now()
+    seconds_passed = (current_time - converted_time).total_seconds()
+    return int(seconds_passed)
+
+
+
