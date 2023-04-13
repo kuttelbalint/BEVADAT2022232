724f6438ca41fca2ba5176e801d8249115ac4764
kuttelbalint <106156156+kuttelbalint@users.noreply.github.com>
2023-04-13 21:07:26 +0200

    updates for hazi

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -1,64 +1,128 @@
-import pandas as pd
+import numpy as np
+from HAZI.HAZI06.NJCleaner import NJCleaner
 
-class NJCleaner:
-    def __init__(self, csv_path : str) -> None:
-        self.data = pd.read_csv(csv_path)
+from src.Node import Node
 
-    def order_by_scheduled_time(self):
-        order = self.data.sort_values(by=['scheduled_time'])
-        return order
+class DecisionTreeClassifier():
+    def __init__(self, min_samples_split=2, max_depth=2):
+        
+        self.root = None
+        
+        self.min_samples_split = min_samples_split 
+        self.max_depth = max_depth
+        
+    def build_tree(self, dataset, curr_depth=0):
+        
+        X, Y = dataset[:,:-1], dataset[:,-1]
+        num_samples, num_features = np.shape(X)
+        if num_samples>=self.min_samples_split and curr_depth<=self.max_depth:
+            best_split = self.get_best_split(dataset, num_samples, num_features)
+            if best_split["info_gain"]>0: 
+                left_subtree = self.build_tree(best_split["dataset_left"], curr_depth+1)
+                right_subtree = self.build_tree(best_split["dataset_right"], curr_depth+1)
+                return Node(best_split["feature_index"], best_split["threshold"],
+                            left_subtree, right_subtree, best_split["info_gain"])
+        
+
+        leaf_value = self.calculate_leaf_value(Y)
+        return Node(value=leaf_value)
     
-    def prep_df(self, path):
-        self.data = self.order_by_scheduled_time()
-
-        self.data = self.order_by_scheduled_time()
-        self.data = self.drop_columns_and_nan()
-        self.data = self.convert_date_to_day()
-        self.data = self.convert_scheduled_time_to_part_of_the_day()
-        self.data = self.convert_delay()
-        self.data = self.drop_unnecessary_columns()
-
-        self.data = self.save_first_60k(path)
-
-        self.data.to_csv(path)
-
-    def drop_columns_and_nan(self):
-        self.data = self.data.drop(['from', 'to'], axis=1).dropna()
-        return self.data
+    def get_best_split(self, dataset, num_samples, num_features):
+        ''' function to find the best split '''
+        
+        best_split = {}
+        max_info_gain = -float("inf")
+        
+        for feature_index in range(num_features):
+            feature_values = dataset[:, feature_index]
+            possible_thresholds = np.unique(feature_values)
+            for threshold in possible_thresholds: 
+                dataset_left, dataset_right = self.split(dataset, feature_index, threshold)
+                if len(dataset_left)>0 and len(dataset_right)>0:
+                    y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
+                    curr_info_gain = self.information_gain(y, left_y, right_y, "gini")
+                    if curr_info_gain>max_info_gain:
+                        best_split["feature_index"] = feature_index
+                        best_split["threshold"] = threshold
+                        best_split["dataset_left"] = dataset_left
+                        best_split["dataset_right"] = dataset_right
+                        best_split["info_gain"] = curr_info_gain
+                        max_info_gain = curr_info_gain
+                        
+        return best_split
     
-    def convert_date_to_day(self):
-        self.data['day'] = pd.to_datetime(self.data['date']).dt.day_name()
-        self.data.drop(columns=['date'], inplace=True)
-        return self.data
+    def split(self, dataset, feature_index, threshold):
+        dataset_left = np.array([row for row in dataset if row[feature_index]<=threshold])
+        dataset_right = np.array([row for row in dataset if row[feature_index]>threshold])
+        return dataset_left, dataset_right
     
-    def convert_scheduled_time_to_part_of_the_day(self):
-        def part_of_the_day(hour):
-            if 4 <= hour < 8:
-                return 'early_morning'
-            elif 8 <= hour < 12:
-                return 'morning'
-            elif 12 <= hour < 16:
-                return 'afternoon'
-            elif 16 <= hour < 20:
-                return 'evening'
-            elif 20 <= hour <= 23:
-                return 'night'
-            else:
-                return 'late_night'
-        self.data['part_of_the_day'] = pd.to_datetime(self.data['scheduled_time']).dt.hour.apply(part_of_the_day)
-        self.data.drop('scheduled_time', axis=1, inplace=True)
+    def information_gain(self, parent, l_child, r_child, mode="entropy"):
+        weight_l = len(l_child) / len(parent)
+        weight_r = len(r_child) / len(parent)
+        if mode=="gini":
+            gain = self.gini_index(parent) - (weight_l*self.gini_index(l_child) + weight_r*self.gini_index(r_child))
+        else:
+            gain = self.entropy(parent) - (weight_l*self.entropy(l_child) + weight_r*self.entropy(r_child))
+        return gain
+    
+    def entropy(self, y):
+        class_labels = np.unique(y)
+        entropy = 0
+        for cls in class_labels:
+            p_cls = len(y[y == cls]) / len(y)
+            entropy += -p_cls * np.log2(p_cls)
+        return entropy
+    
+    def gini_index(self, y):
+        class_labels = np.unique(y)
+        gini = 0
+        for cls in class_labels:
+            p_cls = len(y[y == cls]) / len(y)
+            gini += p_cls**2
+        return 1 - gini
+        
+    def calculate_leaf_value(self, Y):
+        
+        Y = list(Y)
+        return max(Y, key=Y.count)
+    
+    def print_tree(self, tree=None, indent=" "):
+        
+        if not tree:
+            tree = self.root
 
-        return self.data
-    
-    def convert_delay(self):
-        self.data['delay'] = (self.data['real_time'] - self.data['scheduled_arrival']).apply(lambda x: 1 if x >= pd.Timedelta(minutes=5) else 0)
-        return self.data
-    
-    def drop_unnecessary_columns(self):
-        df = self.data.copy()
-        df = df.drop(['train_id', 'actual_time', 'delay_minutes'], axis=1)
+        if tree.value is not None:
+            print(tree.value)
 
-        return df
+        else:
+            print("X_"+str(tree.feature_index), "<=", tree.threshold, "?", tree.info_gain)
+            print("%sleft:" % (indent), end="")
+            self.print_tree(tree.left, indent + indent)
+            print("%sright:" % (indent), end="")
+            self.print_tree(tree.right, indent + indent)
     
-    def save_first_60k(self, path):
-     self.data.iloc[:60000].to_csv(path, index=False)
+    def fit(self, X, Y):
+        
+        dataset = np.concatenate((X, Y), axis=1)
+        self.root = self.build_tree(dataset)
+    
+    def predict(self, X):
+        
+        preditions = [self.make_prediction(x, self.root) for x in X]
+        return preditions
+    
+    def make_prediction(self, x, tree):
+        
+        if tree.value!=None: 
+            return tree.value
+        feature_val = x[tree.feature_index]
+        if feature_val<=tree.threshold:
+            return self.make_prediction(x, tree.left)
+        else:
+            return self.make_prediction(x, tree.right)
+        
+
+cleaner = NJCleaner()
+data = cleaner.prep_df("data/NJ.csv")
+
+print(data)
