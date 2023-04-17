88f221ac1c588b4489d4f4c1efb2d360db3339d6
kuttelbalint <106156156+kuttelbalint@users.noreply.github.com>
2023-04-17 15:46:57 +0200

    paraméterek, leírás

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -1,85 +1,75 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-# load data
-df = pd.read_csv("HAZI/HAZI06/NJ_60k.csv")
+data = pd.read_csv('HAZI/HAZI06/NJ_60k.csv')
 le = LabelEncoder()
-df['status'] = le.fit_transform(df['status'])
-df['line'] = le.fit_transform(df['line'])
-df['type'] = le.fit_transform(df['type'])
-df['day'] = le.fit_transform(df['day'])
-df['part_of_the_day'] = le.fit_transform(df['part_of_the_day'])
-# create feature and target variables
-X = df.drop(['delay'], axis=1)
-y = df['delay']
+data['status'] = le.fit_transform(data['status'])
+data['line'] = le.fit_transform(data['line'])
+data['type'] = le.fit_transform(data['type'])
+data['day'] = le.fit_transform(data['day'])
+data['part_of_the_day'] = le.fit_transform(data['part_of_the_day'])
 
-# split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# create decision tree classifier
-dt_classifier = DecisionTreeClassifier()
+X = data.drop(['delay'], axis=1)
+y = data['delay']
 
-# define hyperparameters grid
-params = {'criterion': ['gini', 'entropy'],
-'max_depth': [None, 2, 4, 6, 8, 10, 12],
-'min_samples_split': [2, 3, 5, 8, 10, 20, 30],
-'min_samples_leaf': [1, 2, 4, 8, 10, 20, 30]}
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
 
-# perform grid search to find best hyperparameters
-grid_search = GridSearchCV(estimator=dt_classifier, param_grid=params, cv=5, verbose=1)
-grid_search.fit(X_train, y_train)
 
-# print best hyperparameters and corresponding accuracy score
-print("Best hyperparameters: ", grid_search.best_params_)
-y_pred = grid_search.predict(X_test)
-print("Accuracy score: ", accuracy_score(y_test, y_pred))
+model = DecisionTreeClassifier( criterion='gini', max_depth=12, min_samples_leaf=10, random_state=41)
+model.fit(X_train, y_train)
 
+
+accuracy = model.score(X_test, y_test)
+(f'Accuracy: {accuracy:.2%}')
 
 """
 10 fitelés eredménye, és azok pontossága:
-1.
-    {'criterion': 'entropy', 'max_depth': 10, 'min_samples_leaf': 8, 'min_samples_split': 2}
-    Accuracy: 0.79775
-
+1. 
+    criterion='entropy', max_depth=12, min_samples_leaf=10, random_state=41
+    Accuracy: 80.19%
 2.
-    {'criterion': 'entropy', 'max_depth': 8, 'min_samples_leaf': 12, 'min_samples_split': 2}
-    Accuracy: 0.7915833333333333
-3. 
-     {'criterion': ['gini'],
-          'max_depth': [4],
-          'min_samples_split': [5],
-          'min_samples_leaf': [1]}
-    Accuracy: 0.7793333333333333
-
+    criterion='entropy', max_depth=18, min_samples_leaf=10, random_state=41
+    Accuracy: 79.47%
+3.
+    criterion='entropy', max_depth=18, min_samples_leaf=5, random_state=41
+    Accuracy: 78.66%
 4.
-    params = {'criterion': ['entropy'],
-          'max_depth': [8],
-          'min_samples_split': [10],
-          'min_samples_leaf': [1]}
-    Accuracy: 0.7915
-
-5. 
-    params = {'criterion': ['gini'],
-          'max_depth': [2],
-          'min_samples_split': [2],
-          'min_samples_leaf': [1]}
-    Accuracy: 0.7745
-
-6. 
-    params = {'criterion': ['entropy'],
-          'max_depth': [6],
-          'min_samples_split': [3],
-          'min_samples_leaf': [2]}
-    Accuracy: 0.7828333333333334
-
+    criterion='gini', max_depth=18, min_samples_leaf=5, random_state=41
+    Accuracy: 78.75%
+5.
+    criterion='gini', max_depth=6, min_samples_leaf=12, random_state=41
+    Accuracy: 78.83%
+6.
+    criterion='gini', max_depth=60, min_samples_leaf=12, random_state=41
+    Accuracy: 79.24%
 7.
-    params = {'criterion': ['entropy'],
-          'max_depth': [100],
-          'min_samples_split': [20],
-          'min_samples_leaf': [80]}
-    Accuracy: 0.7983333333333333
+    criterion='gini', max_depth=60, min_samples_leaf=50, random_state=41
+    Accuracy: 79.96%
+8.
+    criterion='entropy', max_depth=60, min_samples_leaf=66, random_state=41
+    Accuracy: 79.74%
+9. 
+    criterion='gini', max_depth=12, min_samples_leaf=10, random_state=41
+    Accuracy: 80,37%
+10.
+    criterion='gini', max_depth=14, min_samples_leaf=66, random_state=41
+    Accuracy: 79.66
 
+Az általam talált legjobb paramérek: 9. teszteset
+
+
+Összefoglalás:
+
+    Számomra a legnagyobb kihívást a feladat megértése, valamint a decisionTree modell 
+    értelmezése és implementálása jelentett. Elméletben már értettem, hogy hogyan működik, 
+    de így ezen a "komplexebb" gyakorlati példán már nehezebbnek bizonyosult a feladat implementálása
+    python nyelvben. Számos error message-t kaptam, melyek néha nem voltak egyértelműek - köztük a csv betöltésénél, az oszlopok típusánál.
+    Végül a paraméterek megtalálásánál akadtam el hosszabb időre. Végül sikerült olyan paramétereket (random módón:D)
+    találnom, melyek segítségével 80% fölé tudtam vinni az accuracy-t. A következő "célom" az lenne, hogy a paraméterezésnél
+    legyen valamilyen céltudatosságom, jobban megértsem, hogy az egyes paraméterek változtatása milyen irányba viszi majd el
+    a pontosságot.
 """
+
