58c53fe7814fa11c96c9045439fe419f8ba601c8
kuttelbalint <106156156+kuttelbalint@users.noreply.github.com>
2023-05-02 23:53:05 +0200

    hazi09 fix

--- HAZI/HAZI09/HAZI09.py
+++ HAZI/HAZI09/HAZI09.py
@@ -1,25 +1,26 @@
-from sklearn.datasets import load_digits
-from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score, confusion_matrix
-from statistics import mode
-import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
-import statistics
+sns.set()
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from scipy.stats import mode
+from sklearn.metrics import confusion_matrix
+import sklearn.datasets as datasets
 
 
 class KMeansOnDigits:
-    def __init__(self, n_clusters=10, random_state=0):
+    def __init__(self, n_clusters, random_state):
         self.n_clusters = n_clusters
         self.random_state = random_state
         self.digits = None
-        self.model = None
         self.clusters = None
         self.labels = None
         self.accuracy = None
         self.mat = None
 
     def load_dataset(self):
-        self.digits = load_digits()
+        self.digits = datasets.load_digits()
 
     def predict(self):
         self.clusters = KMeans(n_clusters=self.n_clusters, random_state=self.random_state).fit_predict(self.digits.data)
@@ -32,8 +33,8 @@ class KMeansOnDigits:
             self.labels[mask] = mode(self.digits.target[mask])[0]
         return self.labels
 
-    def calc_accuracy(self, target_labels:np.ndarray,predicted_labels:np.ndarray):
-        self.accuracy = accuracy_score(target_labels, predicted_labels)
+    def calc_accuracy(self):
+        self.accuracy = accuracy_score(self.digits.target, self.labels)
         return round(self.accuracy, 2)
 
     def confusion_matrix(self):
